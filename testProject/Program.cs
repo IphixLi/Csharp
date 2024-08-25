3209f85af39ec4cc4d836729e4f92cbc4f89d6f5
IphixLi <iphigeniebera2024@u.northwestern.edu>
2024-08-24 21:23:19 -0700

    foreach iteration

--- testProject/Program.cs
+++ testProject/Program.cs
@@ -1,27 +1,9 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿using System.Runtime.CompilerServices;
 
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
+String[] items=["B123","C234","A345","C15","B177","G3003","C235","B179"];
 
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+foreach(String Item in items){
+if (Item.StartsWith("B")){
+    Console.WriteLine(Item);
+}
 }
