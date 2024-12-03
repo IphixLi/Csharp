43429fa92ccda100e2b7022ebdb31dd4522666e5
IphixLi <iphigeniebera2024@u.northwestern.edu>
2024-12-02 18:51:11 -0800

    alphanumeric ops

--- testProject/Program.cs
+++ testProject/Program.cs
@@ -1,79 +1,73 @@
-﻿
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-// Step 1
-string[] message = orderStream.Split(',');
-Array.Sort(message);
-foreach(String m in message){
-   if (m.Length!=4){
-      Console.WriteLine(m + "\t- Error");
-   }else{
-      Console.WriteLine(m);
-   }
-}
+﻿string customerName = "Ms. Barros";
 
-// ------------------------
-// string pangram = "The quick brown fox jumps over the lazy dog";
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
-// // Step 1
-// string[] message = pangram.Split(' ');
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
-// //Step 2
-// string[] newMessage = new string[message.Length];
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
 
-// // Step 3
-// for (int i = 0; i < message.Length; i++)
-// {
-//     char[] letters = message[i].ToCharArray();
-//     Array.Reverse(letters);
-//     newMessage[i] = new string(letters);
-// }
+Console.WriteLine("Here's a quick comparison:\n");
 
-// //Step 4
-// string result = String.Join(" ", newMessage);
-// Console.WriteLine(result);
-// __________________
-// string value = "abc123";
-// char[] valueArray = value.ToCharArray();
-// Array.Reverse(valueArray);
-// // string result = new string(valueArray);
-// string result = String.Join(",", valueArray);
-// Console.WriteLine(result);
+string comparisonMessage = "";
 
-// string[] items = result.Split(',');
-// foreach (string item in items)
-// {
-//     Console.WriteLine(item);
-// }
-// __________________
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
 
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
 
-// string[] pallets = [ "B14", "A11", "B12", "A13" ];
-// Console.WriteLine("");
+Console.WriteLine(comparisonMessage);
+//  -------------------
+// string input = "Pad this";
+//  Console.WriteLine(input.PadLeft(12));
+//  Console.WriteLine(input.PadLeft(12, '-'));
+//  Console.WriteLine(input.PadRight(12, '-'));
 
-// Array.Clear(pallets, 0, 2);
-// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
+// ------------------
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
 
-// Console.WriteLine("");
-// Array.Resize(ref pallets, 6);
-// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Shares: {productShares:N3} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
 
-// pallets[4] = "C01";
-// pallets[5] = "C02";
+// -----------------------
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine("{1} {0}!", first, second);
+// Console.WriteLine("{0} {0} {0}!", first, second);
 
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
+// // currency
+// // decimal price = 123.45m;
+// // int discount = 50;
+// // Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
-// Console.WriteLine("");
-// Array.Resize(ref pallets, 3);
-// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N} units");
 
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
+// // percentage
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
+
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+
+// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+// yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+// Console.WriteLine(yourDiscount);
